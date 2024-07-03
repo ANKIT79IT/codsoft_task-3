fa1f8e2f2a991a59cd31ab084998878c4597ba2b
ANKIT KUMAR <157393238+ANKIT79IT@users.noreply.github.com>
2024-07-03 09:37:13 +0530

    Add files via upload

---
+++ password generat.py
@@ -0,0 +1,10 @@
+# our second program is password generator.
+import random
+import string
+def generate_password(length):
+  characters = string.ascii_letters + string.digits + string.punctuation
+  password = ''.join(random.choice(characters) for i in range(length))
+  return password
+length = int(input("Enter the desired length of the password: "))
+password = generate_password(length)
+print(f"Your password is: {password}")
